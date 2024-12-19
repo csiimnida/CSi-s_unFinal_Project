6c2e915b0e2831c0c40f3d62774e609272f3ca3d
csi <jjsjuseo@gmail.com>
2024-12-19 16:19:51 +0900

    feat : BackOutScreen

--- Assets/Work/CSI/01.Script/Flage/MouseChugaStage.cs
+++ Assets/Work/CSI/01.Script/Flage/MouseChugaStage.cs
@@ -2,34 +2,58 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseChugaStage : MonoBehaviour ,IRestartable
 {
     public MouseStage mouseStage;
-
+    public Image image;
 
     private void OnEnable()
     {
         if (mouseStage == MouseStage.AncurMouse)
         {
             FollowMouse.Instance.CanFollow = false;
+        }else if (mouseStage == MouseStage.ScreenBlack)
+        {
+            StartCoroutine(ScreenBalckWait());
         }
     }
 
+
+    IEnumerator ScreenBalckWait()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        image.enabled = true;
+    }
     private void OnDisable()
     {
         FollowMouse.Instance.CanFollow = true;
+        if (mouseStage == MouseStage.ScreenBlack)
+        {
+            image.enabled = false;
+        }
+
     }
 
     public enum MouseStage
     {
         AncurMouse,
+        ScreenBlack,
         BlueScreenStage,
     }
 
     public void RestartSet()
     {
-        
+        StopCoroutine(ScreenBalckWait());
+        if (mouseStage == MouseStage.AncurMouse)
+        {
+            FollowMouse.Instance.CanFollow = false;
+        }else if (mouseStage == MouseStage.ScreenBlack)
+        {
+            image.enabled = false;
+            StartCoroutine(ScreenBalckWait());
+        }
     }
 
     public void RestartEnd()
